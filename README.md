55c267bd6c2deff8f3b12e885f3d4ecab1229fac
Jaime Bustos <jaimebustos130@gmail.com>
2023-12-15 11:36:01 -0500

    add more implementation for functions of pygame implement heuristics function format comments for easier reading create 2 dimentional array for grid in make_grid function

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# a-star-pathfinding
+# a-star-pathfinding-visualization
+
+## Description
+
+This project looks at the A* search algorithm in combination with PyGame for visualization. It uses nodes (blocks) that are filled with color to graphically represent positions on a grid. There is a starting and an end point the user is able to place on the graph. Once the points are placed, the user may use the cursor to draw barriers. 
--- astar.py
+++ astar.py
@@ -1,14 +1,14 @@
-import pygame #pygame is used for the visualization of astar
+import pygame # pygame is used for the visualization of astar
 import math
-from queue import PriorityQueue #used for the astar algorithm
+from queue import PriorityQueue # used for the astar algorithm
 
-#defining the window as a square of 800 x 800
+# defining the window as a square of 800 x 800
 WIDTH = 800
 WIN = pygame.display.set_mode((WIDTH, WIDTH))
-#setting the caption for the display
+# setting the caption for the display
 pygame.display.set_caption("A* path finding algorithm")
 
-#defining the color codes to use for visualization
+# defining the color codes to use for visualization
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
@@ -20,18 +20,86 @@ ORANGE = (255, 165 ,0)
 GREY = (128, 128, 128)
 TURQUOISE = (64, 224, 208)
 
-#building the visualization tool
-#each node is each square in the grid
+# building the visualization tool
+# each node is each square in the grid
 class Node:
     def __init__(self, row, col, width, total_rows):
         self.row = row
         self.col = col
+        # we must keep track of coordinate positions
         self.x = row * width
         self.y = col * width
-        self.color = WHITE #we start with all white nodes
-        self.neighbors = []
+        self.color = WHITE # we start with all white nodes
+        self.neighbors = [] 
         self.width = width
         self.total_rows = total_rows
 
+    # this method tells us the position of the node on the grid
     def get_pos(self):
+        # we are indexing by rows and cols, so a node at (6, 3) is at (row: 6, col: 4)
         return self.row, self.col
+    
+    # these methods tell us the state of the node as bool values
+    def is_closed(self): # the node that is already considered by the algorithm
+        return self.color == RED
+
+    def is_open(self):
+        return self.color == GREEN
+    
+    def is_barrier(self): # this node will be black
+        return self.color == BLACK
+    
+    def is_start(self):
+        return self.color == ORANGE
+    
+    def is_end(self):
+        return self.color == TURQUOISE
+    
+    # these methods will change the state of the node
+    def reset(self):
+        self.color == WHITE
+
+    def make_closed(self):
+        self.color = RED
+
+    def make_open(self):
+        self.color = GREEN
+
+    def make_barrier(self):
+        self.color = BLACK
+
+    def make_end(self):
+        self.color = TURQUOISE
+
+    def make_path(self):
+        self.color = PURPLE
+
+    # a function to actually draw the node barriers
+    def draw(self, win):
+        pygame.draw.rec(win, self.color, (self.x, self.y, self.width, self.width))
+
+    def update_neighbors(self, grid):
+        pass
+
+    def __lt__(self, other):
+        return False
+"""
+Now we define the heuristics function
+p1 and p2 are points 1 and 2
+We will use Manhattan distance to determing the distance b/w these points
+"""
+def h(p1, p2):
+    x1, y1 = p1 # for example, p1 = (2, 3)
+    x2, y2 = p2
+    return abs(x1 - x2) + abs(y1 - y2) #
+
+# now a function to build the grid
+def make_grid(rows, width):
+    grid = []
+    gap = width // rows
+    for i in range(rows):
+        grid.append([])
+        for j in range(rows):
+            node = Node(i , j, gap, rows)
+            grid[i].append(node)
+    return grid
